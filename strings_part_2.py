c4fcd80c604e68c7b180a484c47e74040651b33d
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-21 16:43:45 +0530

    Add files via upload

---
+++ strings_part_2.py
@@ -0,0 +1,93 @@
+# a=3
+# b=3.4
+# c='hello'
+# print(type(a))
+# print(type(b))
+# # print(type(c))
+
+# a='hi';b='baby'
+# print(a+b)
+# print(a,b)
+# print(a+""+b)
+# print(a+''''''+b)
+# print(a+' '+b)
+# print(a+'  '+b)
+
+# a='hi' ; b='darling' ; c=3;d=3.4
+# print(a+str(c))
+# print(a+str(d))
+# print(b+str(d))
+# print(str(c))
+# print(str(d))
+# print(int(a))
+# print(float(b))
+# print(float(c))
+# print(a+c)
+# print(b+d)
+
+# a=5;b=5.6;c='love'
+# print(int(b)) #5
+# print(float(a))#5
+# print(int(c))
+# print(float(c))
+
+# x='7'
+# print(type(x))
+# print(int(x))
+# print(float(x))
+
+# y='5.4'
+# print(type(y))
+# # print(int(y))
+# print(float(y))
+# f=float(y)
+# print(int(f))
+
+# print('india')
+# print('love')
+# print('love''india')
+# print('love' 'india')
+# print('love','india')
+# print('love' 'india')
+# print('love'+'india')
+# print('love'*'india')
+# print('love'+''+'india')
+# print('love'+' '+'india')
+# print('love'+'  '+'india')
+# print('love');print('you')
+# print('hi'         'hello')
+# print('hi'
+# 'hello')
+# print('hello'
+
+# 'world')
+
+# print('hi where are you going,may i \
+# come with you')
+
+# print('''helo darling where are you going''')
+# print("""what is your project""")
+# print("""what's your project""")
+
+# print('hi')
+# print()
+# print('baby')
+
+# print('hi'+'hello')
+# print(5*'hello')
+# print(5,*'hello')
+# print(6*'hello ')
+# print(5*,hello)
+# print(4*' hello')
+# print('hello\' *54
+# )
+# print('5'*5)
+# print('5.2 '*6)
+# print('6.4'*5)
+# print('hello'*6.2)
+# print('hello'*'lokesh')
+# print(''+'hello')
+# print(' '+'hello')
+# print(' ','hello')
+# print('','hello')
+# print('  ','hello')
